57276ea6b99385d0f6aadf7e0757bc13acaa91d6
RyanViers <ryanviersiv@hotmail.com>
2022-03-26 14:05:19 -0400

    Added functionality to index.js file.

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-const express = require('express');//Imports express module locally to file.
+const express = require('express'),
+	morgan = require('morgan');//Imports express module locally to file.
+
 const app = express();//Encapsulates Express's functionality to configure web server.
+
+let movies ={"Title":"DUNE"};
+
+app.use(morgan('common'));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+	res.send('This Is The Default Page');
+});
+
+app.get('/movies', (req, res) => {
+	res.json(movies);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(8080, () => {
+	console.log('This app is listening on port 8080');
+});
