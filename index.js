74ca13313b0355e2183f980df8c11cc3bc4b7194
RyanViers <ryanviersiv@hotmail.com>
2022-03-31 10:46:21 -0400

    Added comments.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express'),
-	morgan = require('morgan');//Imports express module locally to file.
+	morgan = require('morgan');//Imports express and morgan modules locally to file.
 
 const app = express();//Encapsulates Express's functionality to configure web server.
 
@@ -9,9 +9,13 @@ let movies =[{"Title":"DUNE","Director":"Denis Villeneuve"},
 {"Title":"Spider Man: No Way Home","Director":"Jon Watts"},
 {"Title":"Walk Hard: The Dewey Cox Story","Director":"Jake Kasdan"}];
 
+/*Morgan Middleware Invoked*/
 app.use(morgan('common'));
+
+/*Serving Static Files*/
 app.use(express.static('public'));
 
+/*GET Requests*/
 app.get('/', (req, res) => {
 	res.send('This Is The Default Page');
 });
@@ -20,11 +24,12 @@ app.get('/movies', (req, res) => {
 	res.json(movies);
 });
 
+/*Error Handeling*/
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
-
+/*App Listener*/
 app.listen(8080, () => {
 	console.log('This app is listening on port 8080');
 });
