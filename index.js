55ead6c14c5f269f68af13573f376ff4d62e2b8c
RyanViers <ryanviersiv@hotmail.com>
2022-05-01 19:55:29 -0400

    CORS

--- index.js
+++ index.js
@@ -42,6 +42,11 @@ app.use(cors());
 		return callback(null, true);
 	}
 }));*/
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 /*****Authentication*****/
 let auth = require("./auth")(app);
