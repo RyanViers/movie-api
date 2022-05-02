843f41d62ad5ff6a892ec4449648f5acf969e669
RyanViers <ryanviersiv@hotmail.com>
2022-05-01 23:47:36 -0400

    cOrs

--- index.js
+++ index.js
@@ -28,10 +28,12 @@ const morgan = require("morgan"), //Imports express and morgan modules locally t
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({
-    origin: '*',
-  })
-);
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Cross-Origin-Resource-Policy", "cross-origin")
+  next()
+})
 
 /*****CORS to limit origins for application*****/
 /*let allowedOrigins = ['/*http://localhost:8080*/ //', '/*http://testsite.com*///'];*/
@@ -61,7 +63,7 @@ const handleError = (error, res) => {
 };
 
 /*app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://ryan-viers-movie-app.herokuapp.com/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });*/
