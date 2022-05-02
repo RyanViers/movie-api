6ef228a3135b67befd7aa9ae2f473f6b54482bb2
RyanViers <ryanviersiv@hotmail.com>
2022-05-02 01:08:56 -0400

    corz

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(cors());
 app.use((req, res, next) => {
   res.header("Cross-Origin-Resource-Policy", "cross-origin");
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Accept");
   next()
 });
 
