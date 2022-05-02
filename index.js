681b785b427678daee2c076e27dcba111f85105d
RyanViers <ryanviersiv@hotmail.com>
2022-05-02 01:47:24 -0400

    t

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use((req, res, next) => {
+app.use(cors(req, res, next) => {
   res.header("Cross-Origin-Resource-Policy", "cross-origin");
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "X-Requested_With");
