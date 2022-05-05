437ec9c76ba18e8b82ad54c61704eabd2b92d10a
RyanViers <ryanviersiv@hotmail.com>
2022-05-05 17:13:51 -0400

    authentication added

--- index.js
+++ index.js
@@ -144,7 +144,9 @@ app.get(
 );
 
 /*READ list of data about all movies.*/
-app.get("/movies", (req, res) => {
+app.get("/movies",
+  passport.authenticate('jwt', { session: false}),
+  (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(200).json(movies);
